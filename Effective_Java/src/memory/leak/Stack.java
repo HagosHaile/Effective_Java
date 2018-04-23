0dc29c3069b22ab999cfa56d1fa9a0a9a4e1bbe9
HagosHaile <hagosb.haile@gmail.com>
2018-04-23 17:45:03 -0400

    code clean up

--- Effective_Java/src/memory/leak/Stack.java
+++ Effective_Java/src/memory/leak/Stack.java
@@ -5,7 +5,6 @@ import java.util.EmptyStackException;
 
 public class Stack {
 
-	
 	private Object[] elements;
     private int size = 0;
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
