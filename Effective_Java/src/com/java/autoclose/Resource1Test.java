25717c898aee6ffd9897b0ff5c72b1bd35e55db1
HagosHaile <hagosb.haile@gmail.com>
2018-04-23 21:23:08 -0400

    try with resource and close method of AutoCloseable

---
+++ Effective_Java/src/com/java/autoclose/Resource1Test.java
@@ -0,0 +1,14 @@
+package com.java.autoclose;
+
+import com.java.autoclose.Resource1;
+
+public class Resource1Test {
+
+	//Resource1 r = new Resource1() is the try with resource part.........
+	public static void main(String[] args) {
+		try (Resource1 r = new Resource1()){
+			System.out.println("Inside Resource 1 Test TRY block.");
+		}
+		System.out.println("Inside Resource 1 Test.");
+	}
+}
