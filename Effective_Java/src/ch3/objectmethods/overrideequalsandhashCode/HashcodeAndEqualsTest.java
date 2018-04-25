328bde7e36f9e69042c40089f669b73092914067
HagosHaile <hagosb.haile@gmail.com>
2018-04-25 16:37:10 -0400

    overriding equals and hsshCode methods erride

---
+++ Effective_Java/src/ch3/objectmethods/overrideequalsandhashCode/HashcodeAndEqualsTest.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package ch3.objectmethods.overrideequalsandhashCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author HAGOS
+ *
+ */
+public class HashcodeAndEqualsTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Map<Employee, String> empMap = new HashMap<Employee, String>();
+		Employee emp1 = new Employee("John", 23, "Bangalore");
+		Employee emp4 = new Employee("John", 23, "Bangalore");
+		Employee emp2 = new Employee("Max", 29, "Chennai");
+		empMap.put(emp1, "John details");
+		empMap.put(emp2, "Max details");
+		System.out.println(emp1);
+		System.out.println(emp2);
+		Employee emp3 = new Employee("John", 23, "Bangalore");
+		System.out.println(empMap.get(emp3));
+		
+		/*
+		 * 
+		The hashcode() and equals() methods contract can be summarized as below;
+		1. If two objects are equal by equals() method then their hash code values must be same.
+		2. If two objects are not equal by equals() method then their hash code may be same or different.
+		***if you override equals methods you must override hashCode method
+		*/
+		System.out.println(emp1.equals(emp2));
+		// this == obj for emp1.equals(emp2) means this is for empl1 and obj is for emp2.
+		System.out.println(emp1.equals(emp4));
+
+	}
+
+}
